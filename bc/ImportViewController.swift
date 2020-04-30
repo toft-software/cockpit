f9d591a72543b93b84c2e6492612fc5a96b9dea6
christian <info@toft-software.dk>
2020-04-30 14:55:53 +0200

    import start

--- bc/ImportViewController.swift
+++ bc/ImportViewController.swift
@@ -10,6 +10,12 @@ protocol SavedPressedDelegate {
 }
 
 
+
+enum selectedFile: Int{
+    case waypoint = 1001, mapobject, matterport, QR, notSelected
+}
+
+
 class ImportViewController: UIViewController{
 
 
@@ -25,7 +31,14 @@ class ImportViewController: UIViewController{
     
     @IBOutlet weak var wayPoints: UITextView!
    
-    public var wayPoint : String;
+    
+    @IBOutlet weak var matterPort: UITextField!
+    @IBOutlet weak var mapObject: UITextField!
+    @IBOutlet weak var wayPoint: UITextField!
+    
+    private var selected : selectedFile!
+    private var child : SpinnerViewController!
+    
     
     @IBAction func saveWaypoint(_ sender: Any) {
         if (searchdelegate != nil) {
@@ -34,48 +47,113 @@ class ImportViewController: UIViewController{
         
     }
     
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
     override func viewDidLoad() {
            super.viewDidLoad()
-           
-           self.wayPoints.text = "te";
-        
-        
-        
+
     }
     
-    
-    
-    
-    
     @IBAction func addMapObjects(_ sender: Any) {
-        
-        
-        
+        self.selected = selectedFile.mapobject;
+        self.DoPick();
     }
     
     @IBAction func addQR(_ sender: Any) {
-        
-        
+        self.selected = selectedFile.QR;
+        self.DoPick();
     }
     
     @IBAction func add3D(_ sender: Any) {
-        
-        
+        self.selected = selectedFile.matterport;
+        self.DoPick();
     }
     
     @IBAction func addWaypoints(_ sender: Any) {
+        self.selected = selectedFile.waypoint;
+        self.DoPick();
+    }
+    
+    func createSpinnerView() {
+        child = SpinnerViewController()
+
+        // add the spinner view controller
+        addChild(child)
+        child.view.frame = view.frame
+        view.addSubview(child.view)
+        child.didMove(toParent: self)
+
+        // wait two seconds to simulate some work happening
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+
+        }
+    }
+    
+    func DoPick () {
         
+        let documentPicker = UIDocumentPickerViewController(documentTypes: ["com.apple.iwork.pages.pages", "com.apple.iwork.numbers.numbers", "com.apple.iwork.keynote.key","public.image", "com.apple.application", "public.item","public.data", "public.content", "public.audiovisual-content", "public.movie", "public.audiovisual-content", "public.video", "public.audio", "public.text", "public.data", "public.zip-archive", "com.pkware.zip-archive", "public.composite-content", "public.text"], in: .import)
+
+        documentPicker.delegate = (self as UIDocumentPickerDelegate)
+        present(documentPicker, animated: true, completion: nil)
+
         
     }
     
+    
+    
 }
 
+extension ImportViewController: UIDocumentPickerDelegate{
 
+
+    func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentAt url: URL) {
+        createSpinnerView();
+              let cico = url as URL
+              print(cico)
+        print(url.absoluteURL)
+
+              print(url.lastPathComponent)
+
+              print(url.pathExtension)
+        self.storeAndShare(withURLString: url.absoluteURL.absoluteString);
+             }
+    
+    /// This function will store your document to some temporary URL and then provide sharing, copying, printing, saving options to the user
+    func storeAndShare(withURLString: String) {
+        guard let url = URL(string : withURLString) else { return }
+        /// START YOUR ACTIVITY INDICATOR HERE
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data, error == nil else { return }
+            let tmpURL = FileManager.default.temporaryDirectory
+                .appendingPathComponent(response?.suggestedFilename ?? "fileName.png")
+            do {
+                try data.write(to: tmpURL)
+            } catch {
+                print(error)
+            }
+            DispatchQueue.main.async {
+                if (self.selected == selectedFile.waypoint)
+                {
+                    self.wayPoint.text = tmpURL.lastPathComponent
+                }
+                else if (self.selected == selectedFile.matterport)
+                {
+                    self.matterPort.text = tmpURL.lastPathComponent
+                }
+                else if (self.selected == selectedFile.mapobject)
+                {
+                    self.mapObject.text = tmpURL.lastPathComponent
+                }
+                else if (self.selected == selectedFile.QR)
+                {
+//self.qr.text = tmpURL.lastPathComponent
+                }
+                            // then remove the spinner view controller
+                self.child.willMove(toParent: nil)
+                self.child.view.removeFromSuperview()
+                self.child.removeFromParent()
+            }
+            }.resume()
+    }
+ }
 
 
 
